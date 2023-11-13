bd882ca863a1fb1e5bd57e09e484d5aa0eb6d9ec
Benjamín Gustavo Martínez Cárdenas <148773184+DevSmitty274@users.noreply.github.com>
2023-11-13 10:14:41 -0300

    Bullet Fix

--- Proyecto Prototipo de Juegos/Assets/Bullet.cs
+++ Proyecto Prototipo de Juegos/Assets/Bullet.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public void Start()
+    {
+        StartCoroutine(BulletDelete());
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Destroy(gameObject);
+    }
+
+    IEnumerator BulletDelete()
+    {
+        yield return new WaitForSecondsRealtime(3);
         Destroy(gameObject);
     }
 }
